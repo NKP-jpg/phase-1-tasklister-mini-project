c53e7111b2787cbdb9e37a7411ec669338b2951e
Nija <nija.perry21@gmail.com>
2021-03-29 17:31:44 -0400

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form')
+  form.addEventListener('submit', taskList)
 });
+
+function taskList(event) {
+  event.preventDefault()
+  // document.getElementsByClassName("create-task-form")
+  const inputField = document.getElementById("new-task-description");
+  console.log(inputField.value);
+  const ul = document.getElementById("tasks");
+  const li = document.createElement("li")
+  li.innerText = inputField.value
+  ul.appendChild(li);
+  inputField.value = ""
+}
+
